e8d5d1baaf0c414a95fb6eff1d30360cc15a1729
Saurav Gupta <sauravgarg540@gmail.com>
2024-02-18 15:17:29 +0100

    add embedding creation

--- main.py
+++ main.py
@@ -2,48 +2,66 @@ import runpod
 import base64
 import os
 import time
-import os
-
 
 runpod.api_key = os.environ.get("API_KEY")
 
-endpoint = runpod.Endpoint(os.environ.get("ENDPOINT"))
-
 def wit_runsync():
-
+    endpoint = runpod.Endpoint(os.environ.get("ENDPOINT"))
 
     start = time.time()
     run_request = endpoint.run_sync(
         {"input":
-             {"text": "Hello, My name is Saurav Gupta. I have just deployed a text2speech model on runpod with run_sync"}
-         }
+            {
+                "text": f"Hello, My name is Saurav Gupta. I have just deployed a text2speech model on runpod with run",
+                "embedding_path": "/runpod-volume/embeddings",
+                "voice_preset": "test4"}}
     )
-    print(time.time()-start)
     audio_name = os.path.basename(run_request["name"])
     with open(audio_name, 'wb') as f:
         ascii = run_request["wav"].encode("ascii")
         bytes_decoded = base64.b64decode(ascii)
         f.write(bytes_decoded)
 
-def with_run(count):
-
+def with_run():
+    endpoint = runpod.Endpoint(os.environ.get("ENDPOINT"))
     run_request = endpoint.run(
         {"input":
-             {"text": f"Hello, My name is Saurav Gupta. I have just deployed a text2speech model on runpod with run {count}"}}
-    )
-
+            {
+                "text": "Hello, My name is Saurav Gupta. I have just deployed a text2speech model on runpod with run",
+                "embedding_path": "/runpod-volume/embeddings",
+                "voice_preset": "test4"}}
+        )
     # Check the status of the endpoint run request
-    print(run_request)
+    print(run_request.job_id)
     print(run_request.status())
 
     # Get the output of the endpoint run request, blocking until the endpoint run is complete.
-    output = run_request.output(timeout=60)
+    output = run_request.output(timeout=200)
     audio_name = os.path.basename(output["name"])
     with open(audio_name, 'wb') as f:
         ascii = output["wav"].encode("ascii")
         bytes_decoded = base64.b64decode(ascii)
         f.write(bytes_decoded)
 
+def generate_embeddding(wav_file_path):
+    endpoint = runpod.Endpoint(os.environ.get("ENDPOINT_A2E"))
+
+    with open(wav_file_path, "rb") as wav_file:
+        # Read the WAV file content
+        wav_content = wav_file.read()
+
+        # Encode the content to base64
+        base64_encoded = base64.b64encode(wav_content).decode('ascii')
+        run_request = endpoint.run_sync(
+            {"input":
+                 {
+                "audio_file": base64_encoded,
+                 "speaker_name": "test4"}
+             }
+        )
+
+        print(run_request)
+
 if __name__ == "__main__":
-    for i in range(5):
-        with_run(i)
+    generate_embeddding("1708204430_tbcvfe.wav")
+    with_run()
